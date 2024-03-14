af51d7533ea4e73919709895d0705a56325768a0
fsampson30 <fsampson30@gmail.com>
2024-03-14 07:50:09 -0300

    Initial commit

---
+++ src/test/java/com/sampson/learnoauth/LearnOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.learnoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
